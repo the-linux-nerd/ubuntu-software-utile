4d7080740d314892a5e590d70c66a2bba3c3dfc3
The Linux Nerd <fabio.linux.nerd@gmail.com>
2024-11-05 20:37:37 +0100

    commit iniziale

---
+++ installa-tutto.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+sudo add-apt-repository universe
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install mc
+sudo apt-get install htop
+sudo apt-get install ncdu
+sudo apt-get install pwgen
+sudo apt-get install gddrescue
+
+sudo apt-get install chromium
+sudo apt-get install gopher
+
+sudo apt-get install filezilla
+
+sudo apt-get install gimp
+sudo apt-get install inkscape
+sudo apt-get install findimagedupes
+sudo apt-get install texlive-fonts-extra
+sudo apt-get install ttf-mscorefonts-installer
+
+sudo apt-get install obs-studio
+sudo apt-get install simplescreenrecorder
+sudo apt-get install audacity
+sudo apt-get install pmidi
+
+sudo apt-get install stopwatch
+
+sudo apt-get install virtualbox
+sudo apt-get install dosbox
+
+sudo apt-get install steam
+
+sudo apt-get install meld
+
+sudo apt-get install libfuse2
+sudo apt-get install libxdo3
+sudo apt-get install libminizip1
+
+sudo apt-get install sqlite3
+sudo apt-get install sqlitebrowser
+
+sudo apt-get install weechat
+
+sudo apt-get install triplea
+
+sudo snap install --classic code
+sudo snap install --classic google-cloud-sdk
+sudo snap install postman
+
+sudo snap install musescore
+sudo snap install spotify
+
+sudo snap install slack
+sudo snap install zoom-client
+sudo snap install discord
+
+sudo snap install arduino
+
+# cose da scaricare e installare
+# Dropbox
+# Rustdesk
